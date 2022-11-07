1bdfd924348963620b16973e092c6d7ec63022b0
TirthWohlig <tirthwohlig@gmail.com>
2022-11-07 13:53:51 +0530

    first commit

---
+++ tp.js
@@ -0,0 +1,12 @@
+function show() {
+    document.getElementById("src").style.display = "block"
+    document.getElementById("x").style.display = "block"
+    document.getElementById("bar").style.display = "none"
+
+}
+function hide() {
+    document.getElementById("src").style.display = "none"
+    document.getElementById("x").style.display = "none"
+    document.getElementById("bar").style.display = "block"
+
+}
