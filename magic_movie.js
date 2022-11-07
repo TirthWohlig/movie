ee88356db46befb88c47c0ef3f4cef3641fda38f
TirthWohlig <tirthwohlig@gmail.com>
2022-11-07 14:12:36 +0530

    first commit

--- magic_movie.js
+++ magic_movie.js
@@ -1,12 +1,13 @@
 function show() {
-    document.getElementById("src").style.display = "block"
-    document.getElementById("x").style.display = "block"
-    document.getElementById("bar").style.display = "none"
-
-}
+    document.getElementById("src").style.display = "block",
+        document.getElementById("x").style.display = "block",
+        document.getElementById("bar").style.display = "none",
+        console.log("Search Bar Shown")
+};
 function hide() {
-    document.getElementById("src").style.display = "none"
-    document.getElementById("x").style.display = "none"
-    document.getElementById("bar").style.display = "block"
+    document.getElementById("src").style.display = "none",
+        document.getElementById("x").style.display = "none",
+        document.getElementById("bar").style.display = "block",
+        console.log("Search Bar Hidden")
 
-}
+};
